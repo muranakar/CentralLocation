8c95382932468f3408674a975753f1ab6244ea78
muranakar <p1211065@gmail.com>
2022-06-11 08:56:02 +0900

    中央座標検出

--- CentralLocation/ViewController.swift
+++ CentralLocation/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiview: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func leftxyLocation(_ sender: Any) {
+        print(uiview.layer)
+    }
+
+    @IBAction func squereSize(_ sender: Any) {
+        print(uiview.frame)
+
+    }
+
+    @IBAction func centerLocation(_ sender: Any) {
+        let x = uiview.frame.origin.x + uiview.frame.width / 2
+        let y = uiview.frame.origin.y + uiview.frame.height / 2
+        print("(x,y)",(x,y))
+     }
 }
 
